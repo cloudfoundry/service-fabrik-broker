90a252072a96c34cd131443ddc999458daaf22df
Basavaraju, Jagadish <jagadish.basavaraju@sap.com>
2017-08-11 21:54:38 +0530

    fixed issue of backup status pollers running even after test execution. Also fixed incorrect usage of setTimeout in test.

--- test/fabrik.DBManager.spec.js
+++ test/fabrik.DBManager.spec.js
@@ -5,8 +5,6 @@ const Promise = require('bluebird');
 const _ = require('lodash');
 const BoshDirectorClient = require('../lib/bosh/BoshDirectorClient');
 const errors = require('../lib/errors');
-const BackupStore = require('../lib/iaas/BackupStoreForServiceInstance');
-const Agent = require('../lib/fabrik/Agent');
 const CONST = require('../lib/constants');
 
 const DirectorManagerStub = {
@@ -68,10 +66,7 @@ describe('fabrik', function () {
   /* jshint expr:true */
   describe('DBManager', function () {
     let dbManager;
-    let sandbox, getDeploymentVMsStub, startBackupStub, putFileStub, getFileStub, startRestoreStub,
-      listBackupFilesStub, getBackupLastOperationStub, getBackupLogsStub, patchBackupFileStub,
-      getRestoreLastOperationStub, getRestoreLogsStub, patchRestoreFileStub, getRestoreFileStub,
-      listLastRestoreFilesStub, getDeploymentStub, pollTaskStatusTillCompleteStub;
+    let sandbox, getDeploymentVMsStub, getDeploymentStub, pollTaskStatusTillCompleteStub;
     const db_backup_guid = '925eb8f4-1e14-42f6-b7cd-cdcf05205bb2';
     const dbIps = ['10.11.0.2', '10.11.0.3', '10.11.0.4'];
     const deploymentVms = [{
@@ -155,36 +150,10 @@ describe('fabrik', function () {
       getDeploymentVMsStub = sandbox.stub(BoshDirectorClient.prototype, 'getDeploymentVms');
       getDeploymentStub = sandbox.stub(BoshDirectorClient.prototype, 'getDeployment');
       pollTaskStatusTillCompleteStub = sandbox.stub(BoshDirectorClient.prototype, 'pollTaskStatusTillComplete');
-      startBackupStub = sandbox.stub(Agent.prototype, 'startBackup');
-      startRestoreStub = sandbox.stub(Agent.prototype, 'startRestore');
-      getBackupLastOperationStub = sandbox.stub(Agent.prototype, 'getBackupLastOperation');
-      getBackupLogsStub = sandbox.stub(Agent.prototype, 'getBackupLogs');
-      getRestoreLastOperationStub = sandbox.stub(Agent.prototype, 'getRestoreLastOperation');
-      getRestoreLogsStub = sandbox.stub(Agent.prototype, 'getRestoreLogs');
-      putFileStub = sandbox.stub(BackupStore.prototype, 'putFile');
-      getFileStub = sandbox.stub(BackupStore.prototype, 'getBackupFile');
-      patchBackupFileStub = sandbox.stub(BackupStore.prototype, 'patchBackupFile');
-      patchRestoreFileStub = sandbox.stub(BackupStore.prototype, 'patchRestoreFile');
-      listBackupFilesStub = sandbox.stub(BackupStore.prototype, 'listBackupFiles');
-      getRestoreFileStub = sandbox.stub(BackupStore.prototype, 'getRestoreFile');
-      listLastRestoreFilesStub = sandbox.stub(BackupStore.prototype, 'listLastRestoreFiles');
       getDeploymentVMsStub.withArgs().returns(Promise.resolve(deploymentVms));
       getDeploymentStub.withArgs('service-fabrik-mongodb-new').returns(deferred.promise);
       getDeploymentStub.withArgs('service-fabrik-mongodb').returns(Promise.resolve({}));
       pollTaskStatusTillCompleteStub.withArgs().returns(Promise.resolve({}));
-      startBackupStub.withArgs().returns(Promise.resolve('10.11.0.2'));
-      startRestoreStub.withArgs().returns(Promise.resolve('10.11.0.2'));
-      getBackupLogsStub.withArgs().returns(Promise.resolve(backup_logs));
-      getBackupLastOperationStub.withArgs().returns(Promise.resolve(backup_state));
-      getRestoreLastOperationStub.withArgs().returns(Promise.resolve(restore_state));
-      getRestoreLogsStub.withArgs().returns(Promise.resolve(restore_logs));
-      getFileStub.withArgs().returns(Promise.resolve(backupMetaData));
-      listBackupFilesStub.withArgs().returns(Promise.resolve([backupMetaData, backupMetaData]));
-      putFileStub.withArgs().returns(Promise.resolve({}));
-      patchBackupFileStub.withArgs().returns(Promise.resolve({}));
-      patchRestoreFileStub.withArgs().returns(Promise.resolve({}));
-      getRestoreFileStub.withArgs().returns(Promise.resolve(restoreMetaData));
-      listLastRestoreFilesStub.withArgs().returns(Promise.resolve([restoreMetaData, restoreMetaData]));
       dbManager = new DBManager();
     });
 
@@ -192,19 +161,6 @@ describe('fabrik', function () {
       getDeploymentStub.reset();
       getDeploymentVMsStub.reset();
       pollTaskStatusTillCompleteStub.reset();
-      startBackupStub.reset();
-      startRestoreStub.reset();
-      getBackupLastOperationStub.reset();
-      getBackupLogsStub.reset();
-      getRestoreLastOperationStub.reset();
-      getRestoreLogsStub.reset();
-      putFileStub.reset();
-      getFileStub.reset();
-      patchBackupFileStub.reset();
-      patchRestoreFileStub.reset();
-      listBackupFilesStub.reset();
-      getRestoreFileStub.reset();
-      listLastRestoreFilesStub.reset();
     });
 
     after(function () {
