846b1f8f2da7ff78fb784229b5a8e449b367b363
amitmalav <amitmalav96@gmail.com>
2017-11-09 11:31:18 +0530

    Add report app
    * Added new app (report) for reporting purposes
    * Added new request route /report dedicated for reporting

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ const internal = FabrikApp.createApp('internal', app => {
 });
 
 // exernal app
+<<<<<<< HEAD
 const external = FabrikApp.createApp('external', app => {
+=======
+const external = ExpressApp.createApp('external', app => {
+>>>>>>> Add report app
   // home
   app.get('/', (req, res) => {
     res.render('index', {
