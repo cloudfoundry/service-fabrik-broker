d5ad9ef76f02a880f31b356186744a9d5f530ba3
i336605 <prasad.kamath01@sap.com>
2018-02-18 12:50:07 +0530

    Made changes based on latest discussion to handle filtering of OOB backups

--- test/iaas.BackupStoreForServiceInstance.spec.js
+++ test/iaas.BackupStoreForServiceInstance.spec.js
@@ -6,7 +6,7 @@ const CloudProviderClient = lib.iaas.CloudProviderClient;
 const backupStore = lib.iaas.backupStore;
 
 describe('iaas', function () {
-  describe('backupStore', function () {
+  describe('backupStoreForServiceInstance', function () {
     describe('listBackupFilenames', function () {
       let sandbox, listStub;
       const deployment_name = 'ccdb-postgresql';
@@ -15,8 +15,8 @@ describe('iaas', function () {
       const space_guid = 'space-guid';
       const service_guid = 'service-guid';
       const instance_guid = 'instance-guid';
-      const oob_backup_started_at_suffix = new Date().toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
-      const service_instance_backup_started_at_suffix = new Date().toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
+      const oob_backup_started_at_suffix = new Date((new Date()).getTime() - 1000 * 600).toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
+      const service_instance_backup_started_at_suffix = new Date((new Date()).getTime() - 1000 * 600).toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
       const operation = "backup";
       before(function () {
         sandbox = sinon.sandbox.create();
@@ -35,14 +35,10 @@ describe('iaas', function () {
       });
 
       it('should list all service instance backup file names', function () {
-        var files = backupStore.listBackupFilenames(Date.now()).then(filenameObject => {
-          expect(filenameObject.service_id).to.equal(service_guid);
-          expect(filenameObject.space_guid).to.equal(space_guid);
-          expect(filenameObject.instance_guid).to.equal(instance_guid);
-          expect(filenameObject.operation).to.equal(operation);
-          expect(filenameObject.started_at).to.equal(service_instance_backup_started_at_suffix.replace(/:/g, '-'));
+        return backupStore.listBackupFilenames(Date.now()).then(filenameObject => {
+          expect(filenameObject).to.have.lengthOf(1);
+          expect(filenameObject[0].space_guid).to.equal(space_guid);
         });
-
       });
     });
   })
