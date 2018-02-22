18aedca6dcd2dbc05e710ec8caf6bf04a98fc683
i336605 <prasad.kamath01@sap.com>
2018-02-22 13:24:01 +0530

    Fixed variable names as per OSBAPI standards

--- test/iaas.BackupStoreForOob.spec.js
+++ test/iaas.BackupStoreForOob.spec.js
@@ -82,7 +82,7 @@ describe('iaas', function () {
       const deployment_name = 'ccdb-postgresql';
       const oob_backup_guid = 'oob-backup-guid';
       const service_instance_backup_guid = 'service-instance-backup-guid';
-      const space_guid = 'space-guid';
+      const tenant_id = 'space-guid';
       const service_guid = 'service-guid';
       const instance_guid = 'instance-guid';
       const oob_backup_started_at_suffix = new Date((new Date()).getTime() - 1000 * 600).toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
@@ -94,7 +94,7 @@ describe('iaas', function () {
         listStub.returns(Promise.resolve([{
           name: `${CONST.FABRIK_OUT_OF_BAND_DEPLOYMENTS.ROOT_FOLDER_NAME}/${operation}/${deployment_name}.${oob_backup_guid}.${oob_backup_started_at_suffix}.json`
         }, {
-          name: `${space_guid}/${operation}/${service_guid}.${instance_guid}.${service_instance_backup_guid}.${service_instance_backup_started_at_suffix}.json`
+          name: `${tenant_id}/${operation}/${service_guid}.${instance_guid}.${service_instance_backup_guid}.${service_instance_backup_started_at_suffix}.json`
         }]));
       });
       afterEach(function () {
--- test/iaas.BackupStoreForServiceInstance.spec.js
+++ test/iaas.BackupStoreForServiceInstance.spec.js
@@ -12,7 +12,7 @@ describe('iaas', function () {
       const deployment_name = 'ccdb-postgresql';
       const oob_backup_guid = 'oob-backup-guid';
       const service_instance_backup_guid = 'service-instance-backup-guid';
-      const space_guid = 'space-guid';
+      const tenant_id = 'space-guid';
       const service_guid = 'service-guid';
       const instance_guid = 'instance-guid';
       const oob_backup_started_at_suffix = new Date((new Date()).getTime() - 1000 * 600).toISOString().replace(/\.\d*/, '').replace(/:/g, '-');
@@ -24,7 +24,7 @@ describe('iaas', function () {
         listStub.returns(Promise.resolve([{
           name: `${CONST.FABRIK_OUT_OF_BAND_DEPLOYMENTS.ROOT_FOLDER_NAME}/${operation}/${deployment_name}.${oob_backup_guid}.${oob_backup_started_at_suffix}.json`
         }, {
-          name: `${space_guid}/${operation}/${service_guid}.${instance_guid}.${service_instance_backup_guid}.${service_instance_backup_started_at_suffix}.json`
+          name: `${tenant_id}/${operation}/${service_guid}.${instance_guid}.${service_instance_backup_guid}.${service_instance_backup_started_at_suffix}.json`
         }]));
       });
       afterEach(function () {
@@ -37,7 +37,7 @@ describe('iaas', function () {
       it('should list all service instance backup file names', function () {
         return backupStore.listBackupFilenames(Date.now()).then(filenameObject => {
           expect(filenameObject).to.have.lengthOf(1);
-          expect(filenameObject[0].space_guid).to.equal(space_guid);
+          expect(filenameObject[0].tenant_id).to.equal(tenant_id);
         });
       });
     });
