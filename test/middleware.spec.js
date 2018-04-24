2a7e46e67eae54fa66ead9881859ff9ea8b99170
Ketaki <ketaki.gadre@sap.com>
2018-04-24 11:04:07 +0530

    Avoid quota check if in cf update-service old and new plans belong to the same sku

--- test/middleware.spec.js
+++ test/middleware.spec.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const Promise = require('bluebird');
 const proxyquire = require('proxyquire');
 const middleware = proxyquire('../lib/middleware', {
   'basic-auth': function (req) {
@@ -8,12 +9,19 @@ const middleware = proxyquire('../lib/middleware', {
   }
 });
 const lib = require('../lib');
+const quota = require('../lib/quota');
+const ServiceFabrikApiController = require('../lib/controllers/ServiceFabrikApiController');
+const config = require('../lib/config');
+const CONST = require('../lib/constants');
+const quotaManager = quota.quotaManager;
+const utils = lib.utils;
 const errors = lib.errors;
 const MethodNotAllowed = errors.MethodNotAllowed;
 const NotFound = errors.NotFound;
 const Unauthorized = errors.Unauthorized;
-const ServiceFabrikApiController = require('../lib/controllers/ServiceFabrikApiController');
-const config = require('../lib/config');
+const BadRequest = errors.BadRequest;
+const Forbidden = errors.Forbidden;
+const PROMISE_WAIT_SIMULATED_DELAY = 2;
 
 class Response {
   constructor() {
@@ -208,5 +216,249 @@ describe('#timeout', function () {
         res => {
           expect(res).to.have.status(503);
         });
+  });
+});
+
+describe('#checkQuota', () => {
+  /* jshint expr:true */
+  const service_id = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
+  const plan_id = 'bc158c9a-7934-401e-94ab-057082a5073f'; // name: 'v1.0-xsmall'
+  const plan_id_update = 'd616b00a-5949-4b1c-bc73-0d3c59f3954a'; // name: 'v1.0-large'
+  const plan_id_major_version_update = 'gd158c9a-7934-401e-94ab-057082a5073e'; // name: 'v2.0-xsmall'
+  const organization_guid = 'b8cbbac8-6a20-42bc-b7db-47c205fccf9a';
+  const notEntitledPlanName = 'v1.0-large';
+  const validQuotaPlanName = 'v2.0-xsmall';
+  const invalidQuotaPlanName = 'v1.0-xsmall';
+  const serviceName = 'blueprint';
+  const invalidServiceName = 'not-a-valid-service';
+  const err = 'Error in calculating quota';
+  const operationParameters = {
+    'service-fabrik-operation': 'token'
+  };
+  const parameters = {
+    foo: 'bar'
+  };
+  let isServiceFabrikOperationStub, isNotPlanUpdateStub, isSameSkuUpdateStub, checkQuotaStub;
+  const checkQuota = middleware.checkQuota();
+  const res = new Response();
+  const next = sinon.spy();
+  const operationsBody = {
+    service_id: service_id,
+    plan_id: plan_id_update,
+    parameters: operationParameters,
+    //context: context,
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id
+    }
+  };
+  const notPlanUpdateBody = {
+    service_id: service_id,
+    plan_id: plan_id,
+    parameters: parameters,
+    //context: context,
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id
+    }
+  };
+  const isSameSkuUpdateBody = {
+    service_id: service_id,
+    plan_id: plan_id_major_version_update,
+    parameters: parameters,
+    //context: context,
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id
+    }
+  };
+  const nonCFContextBody = {
+    service_id: service_id,
+    plan_id: plan_id_update,
+    parameters: parameters,
+    context: {
+      platform: 'kubernetes'
+    },
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id
+    }
+  };
+  const CFContextBody = {
+    service_id: service_id,
+    plan_id: plan_id_update,
+    parameters: parameters,
+    context: {
+      platform: 'cloudfoundry'
+    },
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id
+    }
+  };
+  const validBody = {
+    service_id: service_id,
+    plan_id: plan_id_update,
+    parameters: parameters,
+    context: {
+      platform: 'cloudfoundry'
+    },
+    previous_values: {
+      plan_id: plan_id,
+      service_id: service_id,
+      organization_id: organization_guid
+    }
+  };
+  const notEntitledInstance = {
+    plan: {
+      name: notEntitledPlanName
+    },
+    service: {
+      name: serviceName
+    }
+  };
+  const invalidQuotaInstance = {
+    plan: {
+      name: invalidQuotaPlanName
+    },
+    service: {
+      name: serviceName
+    }
+  };
+  const validQuotaInstance = {
+    plan: {
+      name: validQuotaPlanName
+    },
+    service: {
+      name: serviceName
+    }
+  };
+  const errQuotaInstance = {
+    plan: {
+      name: validQuotaPlanName
+    },
+    service: {
+      name: invalidServiceName
+    }
+  };
+  var req = {
+    method: 'PATCH',
+    path: '/foo',
+    auth: {
+      name: 'user',
+      pass: 'secret'
+    },
+    body: {},
+    instance: {}
+  };
+  beforeEach(function () {
+    isServiceFabrikOperationStub = sinon.stub(utils, 'isServiceFabrikOperation');
+    isServiceFabrikOperationStub.withArgs(operationsBody).returns(true);
+    isNotPlanUpdateStub = sinon.stub(utils, 'isNotPlanUpdate');
+    isNotPlanUpdateStub.withArgs(notPlanUpdateBody).returns(true);
+    isSameSkuUpdateStub = sinon.stub(utils, 'isSameSkuUpdate');
+    isSameSkuUpdateStub.withArgs(isSameSkuUpdateBody).returns(true);
+    checkQuotaStub = sinon.stub(quotaManager, 'checkQuota');
+    checkQuotaStub.withArgs(organization_guid, plan_id_update, notEntitledPlanName, serviceName).returns(Promise.resolve(CONST.QUOTA_API_RESPONSE_CODES.NOT_ENTITLED));
+    checkQuotaStub.withArgs(organization_guid, plan_id_update, invalidQuotaPlanName, serviceName).returns(Promise.resolve(CONST.QUOTA_API_RESPONSE_CODES.INVALID_QUOTA));
+    checkQuotaStub.withArgs(organization_guid, plan_id_update, validQuotaPlanName, serviceName).returns(Promise.resolve(CONST.QUOTA_API_RESPONSE_CODES.VALID_QUOTA));
+  });
+  afterEach(function () {
+    next.reset();
+    res.reset();
+    isServiceFabrikOperationStub.restore();
+    isNotPlanUpdateStub.restore();
+    isSameSkuUpdateStub.restore();
+    checkQuotaStub.restore();
+  });
+  it('should call isServiceFabrikOperation and next', () => {
+    req.body = operationsBody;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.not.have.been.called;
+    expect(isSameSkuUpdateStub).to.not.have.been.called;
+    expect(checkQuotaStub).to.not.have.been.called;
+    expect(next).to.have.been.calledOnce.calledWithExactly();
+  });
+  it('should call isNotPlanUpdate and next', () => {
+    req.body = notPlanUpdateBody;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.not.have.been.called;
+    expect(checkQuotaStub).to.not.have.been.called;
+    expect(next).to.have.been.calledOnce.calledWithExactly();
+  });
+  it('should call isSameSkuUpdate and next', () => {
+    req.body = isSameSkuUpdateBody;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.not.have.been.called;
+    expect(next).to.have.been.calledOnce.calledWithExactly();
+  });
+  it('not CF platform, should call next', () => {
+    req.body = nonCFContextBody;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.not.have.been.called;
+    expect(next).to.have.been.calledOnce.calledWithExactly();
+  });
+  it('CF platform, org id undefined, should call next with BadRequest', () => {
+    req.body = CFContextBody;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.not.have.been.called;
+    expect(next).to.have.been.calledOnce.calledWithExactly(new BadRequest(`organization_id is undefined`));
+  });
+  it('Quota not entitled, should call next with Forbidden', () => {
+    req.body = validBody;
+    req.instance = notEntitledInstance;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.have.been.called;
+    return Promise.delay(PROMISE_WAIT_SIMULATED_DELAY)
+      .then(() => expect(next).to.have.been.calledOnce.calledWithExactly(new Forbidden(`Not entitled to create service instance`)));
+  });
+  it('Quota invalid, should call next with Forbidden', () => {
+    req.body = validBody;
+    req.instance = invalidQuotaInstance;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.have.been.called;
+    return Promise.delay(PROMISE_WAIT_SIMULATED_DELAY)
+      .then(() => expect(next).to.have.been.calledOnce.calledWithExactly(new Forbidden(`Quota is not sufficient for this request`)));
+  });
+  it('Quota valid, should call next', () => {
+    req.body = validBody;
+    req.instance = validQuotaInstance;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.have.been.called;
+    return Promise.delay(PROMISE_WAIT_SIMULATED_DELAY)
+      .then(() => expect(next).to.have.been.calledOnce.calledWithExactly());
+  });
+  it('Quota funtion throws error, should call next with error', () => {
+    checkQuotaStub.withArgs(organization_guid, plan_id_update, validQuotaPlanName, invalidServiceName).returns(Promise.reject(err));
+    req.body = validBody;
+    req.instance = errQuotaInstance;
+    checkQuota(req, res, next);
+    expect(isServiceFabrikOperationStub).to.have.been.calledOnce;
+    expect(isNotPlanUpdateStub).to.have.been.calledOnce;
+    expect(isSameSkuUpdateStub).to.have.been.calledOnce;
+    expect(checkQuotaStub).to.have.been.called;
+    return Promise.delay(PROMISE_WAIT_SIMULATED_DELAY)
+      .then(() => expect(next).to.have.been.calledOnce.calledWithExactly(err));
   });
 });
--- test/utils.spec.js
+++ test/utils.spec.js
@@ -3,6 +3,7 @@
 const lib = require('../lib');
 const DirectorManager = lib.fabrik.DirectorManager;
 const utils = lib.utils;
+const catalog = lib.models.catalog;
 
 describe('utils', function () {
   describe('#deploymentNameRegExp', function () {
@@ -56,6 +57,153 @@ describe('utils', function () {
       expect(utils.getRandomCronForEveryDayAtXHoursInterval(8)).to.eql('1 1,9,17 * * *');
       expect(utils.getRandomCronForEveryDayAtXHoursInterval(7)).to.eql('1 1,8,15,22 * * *');
       expect(utils.getRandomCronForEveryDayAtXHoursInterval(3)).to.eql('1 1,4,7,10,13,16,19,22 * * *');
+    });
+  });
+
+  describe('#isServiceFabrikOperation', function () {
+    /* jshint expr:true */
+    const service_id = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
+    const plan_id_update = 'd616b00a-5949-4b1c-bc73-0d3c59f3954a';
+    var queryParams = {
+      service_id: service_id,
+      plan_id: plan_id_update,
+      previous_values: {
+        service_id: service_id
+      }
+    };
+    it('not a service-fabrik-operation, should return false', function () {
+      queryParams.parameters = {
+        foo: 'bar'
+      };
+      expect(utils.isServiceFabrikOperation(queryParams)).to.be.false;
+    });
+    it('service-fabrik-operation, should return true', function () {
+      queryParams.parameters = {
+        'service-fabrik-operation': 'token'
+      };
+      expect(utils.isServiceFabrikOperation(queryParams)).to.be.true;
+    });
+  });
+
+  describe('#isNotPlanUpdate', function () {
+    /* jshint expr:true */
+    const service_id = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
+    const plan_id = 'bc158c9a-7934-401e-94ab-057082a5073f';
+    const plan_id_update = 'd616b00a-5949-4b1c-bc73-0d3c59f3954a';
+    const parameters = {
+      foo: 'bar'
+    };
+    it('not a plan update (previous_values.plan_id not passed), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id_update,
+        parameters: parameters,
+        previous_values: {
+          service_id: service_id
+        }
+      };
+      expect(utils.isNotPlanUpdate(queryParams)).to.be.true;
+    });
+    it('not a plan update (plan_id not passed), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isNotPlanUpdate(queryParams)).to.be.true;
+    });
+    it('not a plan update (previous_values.plan_id and plan_id are same), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isNotPlanUpdate(queryParams)).to.be.true;
+    });
+    it('plan update (previous_values.plan_id and plan_id are not same), should return false', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id_update,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isNotPlanUpdate(queryParams)).to.be.false;
+    });
+  });
+
+  describe('#isSameSkuUpdate', function () {
+    /* jshint expr:true */
+    const service_id = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
+    const plan_id = 'bc158c9a-7934-401e-94ab-057082a5073f'; // name: 'v1.0-xsmall'
+    const plan_id_major_version_update = 'gd158c9a-7934-401e-94ab-057082a5073e'; // name: 'v2.0-xsmall'
+    const plan_id_update = 'bc158c9a-7934-401e-94ab-057082a5073e'; // name: 'v1.0-small'
+    const parameters = {
+      foo: 'bar'
+    };
+    it('same sku update (previous_values.plan_id not passed), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id_update,
+        parameters: parameters,
+        previous_values: {
+          service_id: service_id
+        }
+      };
+      expect(utils.isSameSkuUpdate(queryParams, catalog)).to.be.true;
+    });
+    it('same sku update (plan_id not passed), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isSameSkuUpdate(queryParams, catalog)).to.be.true;
+    });
+    it('same sku update (previous_values.plan_id sku and plan_id sku are same), should return true', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id_major_version_update,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isSameSkuUpdate(queryParams, catalog)).to.be.true;
+    });
+    it('same sku update (previous_values.plan_id sku and plan_id sku are not same) should return false', function () {
+      var queryParams = {
+        service_id: service_id,
+        plan_id: plan_id_update,
+        parameters: parameters,
+        previous_values: {
+          plan_id: plan_id,
+          service_id: service_id
+        }
+      };
+      expect(utils.isSameSkuUpdate(queryParams, catalog)).to.be.false;
+    });
+  });
+
+  describe('#getSkuNameForPlan', function () {
+    it('should return sku names for plan', function () {
+      expect(utils.getSkuNameForPlan('v1.0-xsmall')).to.eql('-xsmall');
+      expect(utils.getSkuNameForPlan('v1.0-dedicated-xsmall')).to.eql('-dedicated-xsmall');
+      expect(utils.getSkuNameForPlan('v2.0-xsmall')).to.eql('-xsmall');
+      expect(utils.getSkuNameForPlan('v2.0-dedicated-xsmall')).to.eql('-dedicated-xsmall');
     });
   });
 });
