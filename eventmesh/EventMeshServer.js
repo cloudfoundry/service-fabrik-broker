26fb14864db0a14d0db368572aa94b01d972f47f
Murali Suresh <muralisc@gmail.com>
2018-05-11 18:33:46 +0530

    Update require

--- eventmesh/EventMeshServer.js
+++ eventmesh/EventMeshServer.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
-const CONST = require('../broker/lib/constants');
+const CONST = require('../common/constants');
 const errors = require('../common/errors');
 const NotImplementedBySubclass = errors.NotImplementedBySubclass;
 
