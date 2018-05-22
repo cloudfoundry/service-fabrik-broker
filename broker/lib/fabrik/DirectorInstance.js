74c8e9669371b547488e67b57b590a10012b8fd8
Jayesh Kshirsagar <jayeshk1992@gmail.com>
2018-05-22 23:16:22 +0530

    Modified getInfo of DirectorInstance to get current plan from CC for dashboard rendering

--- broker/lib/fabrik/DirectorInstance.js
+++ broker/lib/fabrik/DirectorInstance.js
@@ -10,6 +10,7 @@ const logger = require('../logger');
 const errors = require('../errors');
 const jwt = require('../jwt');
 const utils = require('../utils');
+const catalog = require('../models').catalog;
 const NotFound = errors.NotFound;
 const ServiceInstanceNotFound = errors.ServiceInstanceNotFound;
 const ScheduleManager = require('../jobs');
@@ -321,10 +322,19 @@ class DirectorInstance extends BaseInstance {
         this.cloudController.getServiceInstance(this.guid),
         this.initialize(operation).then(() => this.manager.getDeploymentInfo(this.deploymentName))
       ])
-      .spread((instance, deploymentInfo) => ({
-        title: `${this.plan.service.metadata.displayName || 'Service'} Dashboard`,
-        plan: this.plan,
-        service: this.plan.service,
+      .spread((instance,deploymentInfo) => {
+        return Promise.all([
+          instance,
+          deploymentInfo,
+          this.cloudController.getServicePlan(instance.entity.service_plan_guid,{})
+        ]);
+      })
+      .spread((instance, deploymentInfo, planInfo) => {
+        var currentPlan = catalog.getPlan(planInfo.entity.unique_id);
+        return {
+        title: `${currentPlan.service.metadata.displayName || 'Service'} Dashboard`,
+        plan: currentPlan,
+        service: currentPlan.service,
         instance: _.set(instance, 'task', deploymentInfo),
         files: [{
           id: 'status',
@@ -332,7 +342,8 @@ class DirectorInstance extends BaseInstance {
           language: 'yaml',
           content: yaml.dump(deploymentInfo)
         }]
-      }));
+      };
+    });
   }
 
   scheduleBackUp() {
