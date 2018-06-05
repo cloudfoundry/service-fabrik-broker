dac04c54bce70405d472a7c8b4c0453d62578e21
amitmalav <amitmalav96@gmail.com>
2018-06-05 22:35:57 +0530

    Add UT

--- test/test_broker/acceptance/service-fabrik-api.info.spec.js
+++ test/test_broker/acceptance/service-fabrik-api.info.spec.js
@@ -2,6 +2,7 @@
 
 const _ = require('lodash');
 const assert = require('assert');
+const config = require('../../../common/config');
 const app = require('../support/apps').external;
 
 describe('service-fabrik-api', function () {
@@ -17,6 +18,23 @@ describe('service-fabrik-api', function () {
         .get(`${baseUrl}/info`)
         .catch(err => err.response)
         .then(res => {
+          assert.ok(res.body.db_status, 'Service fabrik info must return back db status');
+          expect(res).to.have.status(200);
+          expect(_.omit(res.body, 'db_status')).to.be.eql({
+            name: 'service-fabrik-broker',
+            api_version: '1.0',
+            ready: true
+          });
+          mocks.verify();
+        });
+    });
+    it('returns 200 Ok if swarm is disabled', function () {
+      config.enable_swarm_manager = false;
+      return chai.request(app)
+        .get(`${baseUrl}/info`)
+        .catch(err => err.response)
+        .then(res => {
+          config.enable_swarm_manager = true;
           assert.ok(res.body.db_status, 'Service fabrik info must return back db status');
           expect(res).to.have.status(200);
           expect(_.omit(res.body, 'db_status')).to.be.eql({
