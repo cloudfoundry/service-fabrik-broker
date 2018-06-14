363b7f87d009c7dd046461313c6a14d4d9798d52
i068838 <s.chattopadhyay@sap.com>
2018-06-14 11:20:55 +0530

    Changing the font for the generated seq diag

--- bin/build-architecture-seqdiag.sh
+++ bin/build-architecture-seqdiag.sh
@@ -7,8 +7,8 @@ if [[ ! -z $2 ]]; then
 fi
 
 # Generate sequence diagram png image
-seqdiag -Tpng --no-transparency "${1}" $output && \
+seqdiag -Tpng --no-transparency -f '/Library/Fonts/Verdana.ttf' "${1}" $output && \
   echo -e "Sequence diagram image created and stored in ${target}."
 
 # -f '/Library/Fonts/Verdana.ttf' 
-# Use the above font option to override the fonts.
+# Use the above font option to override the fonts on Mac
