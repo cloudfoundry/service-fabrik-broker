d8cba63c6a28b8ea4d10c998781fc012868bf3b0
Murali Suresh <muralisc@gmail.com>
2018-06-20 18:54:06 +0530

    Trigger based on only change state

--- managers/backup-manager/BackupWatcher.js
+++ managers/backup-manager/BackupWatcher.js
@@ -17,37 +17,37 @@ class DefaultBackupManager extends BaseManager {
   registerWatcher() {
     logger.info(`Registering Backup watcher`);
     return eventmesh.server.registerWatcher('backup', 'defaultbackup', this.worker)
-      .catch(e => logger.error('Caught errro', e));
   }
 
   worker(change) {
-    logger.info('resource changed', change);
-    const value = change.object.spec.options;
-    logger.info('Changed value:', value);
-    const changedValue = JSON.parse(value);
-    logger.info('Parsed value:', changedValue);
-    if (change.type === 'MODIFIED' && change.object.status.state === CONST.APISERVER.RESOURCE_STATE.IN_QUEUE) {
-      logger.info('Triggering backup:', changedValue);
+    logger.info('Changed Resource:', change);
+    logger.debug('Changed resource options:', change.object.spec.options);
+    const changedOptions = JSON.parse(change.object.spec.options);
+    logger.debug('Changed resource options(parsed):', changedOptions);
+    if (change.object.status.state === CONST.APISERVER.RESOURCE_STATE.IN_QUEUE) {
+      logger.info('Triggering backup with the following options:', changedOptions);
       return Promise.try(() => {
-          const plan = catalog.getPlan(changedValue.plan_id);
+          const plan = catalog.getPlan(changedOptions.plan_id);
           return fabrik.createManager(plan);
-        }).then(manager => manager.startBackup(changedValue))
-        .catch(e => logger.error('Caught error while starting backup', e));
+        }).then(manager => manager.startBackup(changedOptions))
+        .catch(e => {
+          logger.error(`Caught error while starting backup ${changedOptions.guid}`, e);
+          throw e
+        });
     } else if (change.object.status.state === CONST.OPERATION.ABORT) {
       logger.info(`State key is set to abort. Triggering abort`);
-      const opts = {
-        resourceId: changedValue.instance_guid,
-        annotationName: CONST.APISERVER.ANNOTATION_NAMES.BACKUP,
-        annotationType: CONST.APISERVER.ANNOTATION_TYPES.BACKUP,
-        annotationId: changedValue.guid
-      };
-      return eventmesh.server.getAnnotationOptions(opts)
+      return eventmesh.server.getAnnotationOptions({
+          resourceId: changedOptions.instance_guid,
+          annotationName: CONST.APISERVER.ANNOTATION_NAMES.BACKUP,
+          annotationType: CONST.APISERVER.ANNOTATION_TYPES.BACKUP,
+          annotationId: changedOptions.guid
+        })
         .then(options => {
-          const changedValue = JSON.parse(options);
+          const changedOptions = JSON.parse(options);
           return Promise.try(() => {
-            const plan = catalog.getPlan(changedValue.plan_id);
+            const plan = catalog.getPlan(changedOptions.plan_id);
             return fabrik.createManager(plan);
-          }).then(manager => manager.abortLastBackup(changedValue));
+          }).then(manager => manager.abortLastBackup(changedOptions));
         }).catch(err => logger.info('Caught error', err));
     }
   }
