52f0564cf9cba1a81c91d9cc380372eac40e9ebc
Murali Suresh <muralisc@gmail.com>
2018-06-29 10:22:01 +0530

    Add user

--- broker/lib/controllers/ServiceFabrikApiController.js
+++ broker/lib/controllers/ServiceFabrikApiController.js
@@ -367,6 +367,8 @@ class ServiceFabrikApiController extends FabrikBaseController {
           instance_guid: req.params.instance_id,
           plan_id: req.body.plan_id || planDetails.entity.unique_id,
           service_id: req.body.service_id || this.getPlan(planDetails.entity.unique_id).service.id,
+          username: req.user.name,
+          useremail: req.user.email || '',
           context: context
         };
         return backupOptions;
