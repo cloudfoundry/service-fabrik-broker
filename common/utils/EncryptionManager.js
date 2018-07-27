f061819598644e28fbe39365998f0ecc0bb1e5dd
i068838 <s.chattopadhyay@sap.com>
2018-07-27 10:09:50 +0530

    Keeping the encryption algorithm constant

--- common/utils/EncryptionManager.js
+++ common/utils/EncryptionManager.js
@@ -6,24 +6,14 @@ const CONST = require('../constants');
 
 class EncryptionManager {
 
-  /**
-   * @param text test to be encrypted
-   */
   encrypt(text) {
-    const cipher = crypto.createCipheriv(CONST.ENCRYPTION.AES_256_ALGORITHM, config.apiserver.encryption.password, config.apiserver.encryption.initialization_vector);
-    let crypted = cipher.update(text, CONST.ENCRYPTION.INPUT_ENCODING, CONST.ENCRYPTION.OUTPUT_ENCODING);
-    crypted += cipher.final(CONST.ENCRYPTION.OUTPUT_ENCODING);
-    return crypted;
+    const cipher = crypto.createCipheriv(CONST.ENCRYPTION.AES_256_ALGORITHM, config.apiserver.encryption.key, config.apiserver.encryption.initialization_vector);
+    return cipher.update(text, CONST.ENCRYPTION.INPUT_ENCODING, CONST.ENCRYPTION.OUTPUT_ENCODING);
   }
 
-  /**
-   * @param text test to be decrypted
-   */
   decrypt(text) {
-    var decipher = crypto.createDecipheriv(CONST.ENCRYPTION.AES_256_ALGORITHM, config.apiserver.encryption.password, config.apiserver.encryption.initialization_vector);
-    let dec = decipher.update(text, CONST.ENCRYPTION.OUTPUT_ENCODING, CONST.ENCRYPTION.INPUT_ENCODING);
-    dec += decipher.final(CONST.ENCRYPTION.INPUT_ENCODING);
-    return dec;
+    var decipher = crypto.createDecipheriv(CONST.ENCRYPTION.AES_256_ALGORITHM, config.apiserver.encryption.key, config.apiserver.encryption.initialization_vector);
+    return decipher.update(text, CONST.ENCRYPTION.OUTPUT_ENCODING, CONST.ENCRYPTION.INPUT_ENCODING);
   }
 
 }
