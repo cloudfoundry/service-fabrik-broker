75fea686a877125433494e8338f142a114f330b2
i068838 <s.chattopadhyay@sap.com>
2018-08-11 19:10:14 +0530

    Test fix

--- test/test_broker/eventmesh.ApiServerClient.spec.js
+++ test/test_broker/eventmesh.ApiServerClient.spec.js
@@ -745,23 +745,5 @@ describe('eventmesh', () => {
       });
     });
 
-    describe('##regWatcher', () => {
-      it('Should register watch on a resource', () => {
-        let handlerCalled = false;
-
-        function handler() {
-          handlerCalled = true;
-        }
-        return apiserver.registerWatcher(CONST.APISERVER.RESOURCE_GROUPS.LOCK,
-            CONST.APISERVER.RESOURCE_TYPES.DEPLOYMENT_LOCKS, handler)
-          .tap(stream => stream.write('{"foo":1}'))
-          .then(stream => {
-            verify();
-            expect(handlerCalled).to.eql(true);
-            expect(stream).to.not.eql(null);
-          });
-      });
-    });
-
   });
 });
---
+++ test/test_broker/watcher.ApiServerClientWatcher.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const apiserver = require('../../data-access-layer/eventmesh').apiServerClient;
+const CONST = require('../../common/constants');
+
+describe('WatcherRegistration', () => {
+  describe('##regWatcher', () => {
+    it('Should register watch on a resource', () => {
+      let handlerCalled = false;
+
+      function handler() {
+        handlerCalled = true;
+      }
+      return apiserver.registerWatcher(CONST.APISERVER.RESOURCE_GROUPS.LOCK,
+          CONST.APISERVER.RESOURCE_TYPES.DEPLOYMENT_LOCKS, handler)
+        .tap(stream => stream.write('{"foo":1}'))
+        .then(stream => {
+          expect(handlerCalled).to.eql(true);
+          expect(stream).to.not.eql(null);
+        });
+    });
+  });
+});
