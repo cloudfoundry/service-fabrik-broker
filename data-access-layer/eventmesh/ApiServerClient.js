900f9072119e006d78b61cf6b23ff8be54db6b80
i068838 <s.chattopadhyay@sap.com>
2018-08-14 11:08:56 +0530

    Changing the log level for polling

--- data-access-layer/eventmesh/ApiServerClient.js
+++ data-access-layer/eventmesh/ApiServerClient.js
@@ -102,7 +102,7 @@ class ApiServerClient {
         const state = resource.status.state;
         if (state === opts.start_state) {
           const duration = (new Date() - opts.started_at) / 1000;
-          logger.info(`Polling for ${opts.start_state} duration: ${duration} `);
+          logger.debug(`Polling for ${opts.start_state} duration: ${duration} `);
           if (duration > CONST.BACKUP.BACKUP_START_TIMEOUT_IN_SECS) {
             logger.error(`Backup with guid ${opts.resourceId} not picked up from the queue`);
             throw new Timeout(`Backup with guid ${opts.resourceId} not picked up from the queue`);
