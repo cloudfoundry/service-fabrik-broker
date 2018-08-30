2f335b807a2864dced7017e82df30e0bf44bd142
Murali Suresh <muralisc@gmail.com>
2018-08-30 13:01:58 +0530

    Add eventlogging in Restore Task poller

--- managers/restore-manager/RestoreTaskPoller.js
+++ managers/restore-manager/RestoreTaskPoller.js
@@ -6,26 +6,29 @@ const eventmesh = require('../../data-access-layer/eventmesh');
 const CONST = require('../../common/constants');
 const logger = require('../../common/logger');
 const catalog = require('../../common/models').catalog;
+const config = require('../common/config');
 const RestoreService = require('./');
-
+const utils = require('../common/utils');
+const EventLogInterceptor = require('../common/EventLogInterceptor');
 
 class RestoreTaskPoller {
 
   static start() {
     function poller(object, interval) {
       // TODO handle HA scenario
-      const response = JSON.parse(_.get(object.status,'response'));
+      const response = JSON.parse(_.get(object.status, 'response'));
       const changedOptions = JSON.parse(object.spec.options);
       logger.info('Getting operation status with the following options and response:', changedOptions, response);
       const plan = catalog.getPlan(changedOptions.plan_id);
+      const restore_opts = {
+        context: changedOptions.context,
+        agent_ip: response.agent_ip,
+        instance_guid: changedOptions.instance_guid,
+        restore_guid: changedOptions.restore_guid
+      };
       return RestoreService.createService(plan)
         .then(restoreService => restoreService
-          .getRestoreOperationState({
-            context: changedOptions.context,
-            agent_ip: response.agent_ip,
-            instance_guid: changedOptions.instance_guid,
-            restore_guid: changedOptions.restore_guid
-          })
+          .getRestoreOperationState(restore_opts)
           .catch(e => logger.error('Caught error in poller', e))
           .then(operationStatusResponse => {
             logger.info('Got restore operation response', operationStatusResponse);
@@ -34,15 +37,21 @@ class RestoreTaskPoller {
               clearInterval(interval);
               RestoreTaskPoller.pollers[object.metadata.name] = false;
             }
-            return eventmesh.apiServerClient.patchResource({
-              resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.RESTORE,
-              resourceType: CONST.APISERVER.RESOURCE_TYPES.DEFAULT_RESTORE,
-              resourceId: changedOptions.restore_guid,
-              status: {
-                'state': operationStatusResponse.state,
-                response: operationStatusResponse
-              }
-            });
+            return Promise
+              .try(() => {
+                if (utils.isServiceFabrikOperationFinished(operationStatusResponse.state)) {
+                  return this._logEvent(restore_opts, CONST.OPERATION_TYPE.RESTORE, operationStatusResponse);
+                }
+              })
+              .then(() => eventmesh.apiServerClient.patchResource({
+                resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.RESTORE,
+                resourceType: CONST.APISERVER.RESOURCE_TYPES.DEFAULT_RESTORE,
+                resourceId: changedOptions.restore_guid,
+                status: {
+                  'state': operationStatusResponse.state,
+                  response: operationStatusResponse
+                }
+              }));
           })
         );
     }
@@ -78,7 +87,20 @@ class RestoreTaskPoller {
         throw e;
       });
   }
+
+  static _logEvent(instanceInfo, operation, operationStatusResponse) {
+    const eventLogger = EventLogInterceptor.getInstance(config.external.event_type, 'external');
+    const check_res_body = true;
+    const resp = {
+      statusCode: 200,
+      body: operationStatusResponse
+    };
+    if (CONST.URL[operation]) {
+      return eventLogger.publishAndAuditLogEvent(CONST.URL[operation], CONST.HTTP_METHOD.POST, instanceInfo, resp, check_res_body);
+    }
+  }
 }
+
 
 RestoreTaskPoller.pollers = [];
 module.exports = RestoreTaskPoller;
