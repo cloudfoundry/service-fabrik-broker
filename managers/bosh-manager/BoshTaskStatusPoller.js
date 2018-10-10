44b9f04151c9b6e89e697f6713007f27b3812a1b
i350504 <jayeshk1992@gmail.com>
2018-10-11 01:06:40 +0530

    modified BoshTaskStatusPoller to add deployment IPs in resource

--- managers/bosh-manager/BoshTaskStatusPoller.js
+++ managers/bosh-manager/BoshTaskStatusPoller.js
@@ -34,33 +34,77 @@ class BoshTaskStatusPoller extends BaseStatusPoller {
     const instanceId = resourceBody.metadata.name;
     const options = _.get(resourceBody, 'spec.options');
     return DirectorService.createInstance(instanceId, options)
-      .then(directorService => directorService.lastOperation(_.get(resourceBody, 'status.response')))
-      .tap(lastOperationValue => logger.debug('last operation value is ', lastOperationValue))
-      .tap(lastOperationValue => lastOperationOfInstance = lastOperationValue)
-      .then(lastOperationValue => Promise.all([eventmesh.apiServerClient.updateResource({
-        resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
-        resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
-        resourceId: instanceId,
-        status: {
-          lastOperation: lastOperationValue,
-          state: lastOperationValue.resourceState
-        }
-      }), Promise.try(() => {
-        if (_.includes([CONST.APISERVER.RESOURCE_STATE.SUCCEEDED, CONST.APISERVER.RESOURCE_STATE.FAILED], lastOperationValue.resourceState)) {
-          // cancel the poller and clear the array
+      .then(directorService => directorService.lastOperation(_.get(resourceBody, 'status.response'))
+        .tap(lastOperationValue => logger.debug('last operation value is ', lastOperationValue))
+        .tap(lastOperationValue => lastOperationOfInstance = lastOperationValue)
+        .then(lastOperationValue => Promise.all([
+          Promise.try(() => {
+            if (lastOperationValue.resourceState === CONST.APISERVER.RESOURCE_STATE.SUCCEEDED &&
+              (lastOperationValue.type === 'create' || lastOperationValue.type === 'update')) {
+              return directorService.director.getDeploymentNameForInstanceId(directorService.guid)
+                .then(deploymentName => directorService.director.getDeploymentIpsFromDirector(deploymentName))
+                .then(ips => eventmesh.apiServerClient.updateResource({
+                  resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
+                  resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
+                  resourceId: instanceId,
+                  status: {
+                    lastOperation: lastOperationValue,
+                    state: lastOperationValue.resourceState
+                  },
+                  metadata: {
+                    annotations: {
+                      deploymentIps: JSON.stringify(ips)
+                    }
+                  }
+                }));
+            } else {
+              return eventmesh.apiServerClient.updateResource({
+                resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
+                resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
+                resourceId: instanceId,
+                status: {
+                  lastOperation: lastOperationValue,
+                  state: lastOperationValue.resourceState
+                }
+              });
+            }
+          }),
+          Promise.try(() => {
+            if (_.includes([CONST.APISERVER.RESOURCE_STATE.SUCCEEDED, CONST.APISERVER.RESOURCE_STATE.FAILED], lastOperationValue.resourceState)) {
+              // cancel the poller and clear the array
+              this.clearPoller(instanceId, intervalId);
+            }
+          })
+        ]))
+        .catch(ServiceInstanceNotFound, err => {
+          logger.error(`Error occured while getting last operation`, err);
           this.clearPoller(instanceId, intervalId);
-        }
-      })]))
-      .catch(ServiceInstanceNotFound, err => {
-        logger.error(`Error occured while getting last operation`, err);
-        this.clearPoller(instanceId, intervalId);
-        if (resourceBody.status.response.type === 'delete') {
-          return eventmesh.apiServerClient.deleteResource({
-            resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
-            resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
-            resourceId: instanceId
-          });
-        } else {
+          if (resourceBody.status.response.type === 'delete') {
+            return eventmesh.apiServerClient.deleteResource({
+              resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
+              resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
+              resourceId: instanceId
+            });
+          } else {
+            lastOperationOfInstance = {
+              state: CONST.APISERVER.RESOURCE_STATE.FAILED,
+              description: CONST.SERVICE_BROKER_ERR_MSG
+            };
+            return eventmesh.apiServerClient.updateResource({
+              resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
+              resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
+              resourceId: instanceId,
+              status: {
+                lastOperation: lastOperationOfInstance,
+                state: CONST.APISERVER.RESOURCE_STATE.FAILED,
+                error: utils.buildErrorJson(err)
+              }
+            });
+          }
+        })
+        .catch(AssertionError, err => {
+          logger.error(`Error occured while getting last operation for instance ${instanceId}`, err);
+          this.clearPoller(instanceId, intervalId);
           lastOperationOfInstance = {
             state: CONST.APISERVER.RESOURCE_STATE.FAILED,
             description: CONST.SERVICE_BROKER_ERR_MSG
@@ -75,26 +119,8 @@ class BoshTaskStatusPoller extends BaseStatusPoller {
               error: utils.buildErrorJson(err)
             }
           });
-        }
-      })
-      .catch(AssertionError, err => {
-        logger.error(`Error occured while getting last operation for instance ${instanceId}`, err);
-        this.clearPoller(instanceId, intervalId);
-        lastOperationOfInstance = {
-          state: CONST.APISERVER.RESOURCE_STATE.FAILED,
-          description: CONST.SERVICE_BROKER_ERR_MSG
-        };
-        return eventmesh.apiServerClient.updateResource({
-          resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
-          resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
-          resourceId: instanceId,
-          status: {
-            lastOperation: lastOperationOfInstance,
-            state: CONST.APISERVER.RESOURCE_STATE.FAILED,
-            error: utils.buildErrorJson(err)
-          }
-        });
-      })
+        })
+      )
       .finally(() => {
         if (_.get(resourceBody.status.response, 'type') === CONST.OPERATION_TYPE.UPDATE &&
           _.get(resourceBody.status.response, 'parameters.service-fabrik-operation') === true &&
