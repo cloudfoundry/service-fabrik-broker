30ba6c7c909936aae887d095f0460ee03aff967c
Saurav Mondal <saurav.mondal@sap.com>
2018-10-16 17:50:03 +0530

    Minor change for operator module import

--- operators/StartBoshOperators.js
+++ operators/StartBoshOperators.js
@@ -3,7 +3,7 @@
 const BoshOperator = require('./bosh-operator/BoshOperator');
 const BoshBindOperator = require('./bosh-operator/BoshBindOperator');
 const BoshTaskStatusPoller = require('./bosh-operator/BoshTaskStatusPoller');
-const BoshStaggeredDeploymentPoller = require('./bosh-manager/BoshStaggeredDeploymentPoller');
+const BoshStaggeredDeploymentPoller = require('./bosh-operator/BoshStaggeredDeploymentPoller');
 
 const boshOperator = new BoshOperator();
 const boshBindOperator = new BoshBindOperator();
