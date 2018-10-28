ea76485a05f7fc9a88695a480ee36e7563aeb580
i336605 <prasad.kamath01@sap.com>
2018-10-28 13:58:40 +0530

    Added unit tests

--- test/test_broker/acceptance/service-fabrik-admin.updates.spec.js
+++ test/test_broker/acceptance/service-fabrik-admin.updates.spec.js
@@ -29,8 +29,7 @@ for (let i = 1; i <= numOfInstances; i++) {
 
 class Repository {
   static search(model, searchCriteria, paginateOpts) {
-    let returnedList = [];
-    {
+    let returnedList = []; {
       returnedList = listOfInstances;
       repositoryStub.search(arguments);
       return Promise.try(() => {
@@ -49,64 +48,68 @@ class Repository {
 describe('service-fabrik-admin', function () {
   const base_url = '/admin';
 
-    describe('#getScheduledUpdateInstances', function () {
+  describe('#getScheduledUpdateInstances', function () {
 
-      before(function () {
-        sinon.stub(ServiceFabrikAdminController, 'getInstancesWithUpdateScheduled').returns(Promise.resolve([{instance_id : '9999-8888-7777-6666'},{instance_id : '5555-4444-3333-2222'}]));
-      });
-    
-      it.only('should list all instances with updates scheduled', function () {
-        return chai
-          .request(app)
-          .get(`${base_url}/update/schedules`)
-          .set('Accept', 'application/json')
-          .auth(config.username, config.password)
-          .catch(err => err.response)
-          .then(res => {
-            expect(res.body).to.have.length(2);
-            expect(res).to.have.status(200);
-          });
-      });
-      });
+    before(function () {
+      sinon.stub(ServiceFabrikAdminController, 'getInstancesWithUpdateScheduled').returns(Promise.resolve([{
+        instance_id: '9999-8888-7777-6666'
+      }, {
+        instance_id: '5555-4444-3333-2222'
+      }]));
+    });
 
-      describe('#getInstancesWithUpdateScheduled', function () {
-        const adminController = proxyquire('../../../api-controllers/ServiceFabrikAdminController', {
-          '../common/db': {
-            Repository: Repository
-          }
+    it.only('should list all instances with updates scheduled', function () {
+      return chai
+        .request(app)
+        .get(`${base_url}/update/schedules`)
+        .set('Accept', 'application/json')
+        .auth(config.username, config.password)
+        .catch(err => err.response)
+        .then(res => {
+          expect(res.body).to.have.length(2);
+          expect(res).to.have.status(200);
         });
-        let repoSpy = sinon.stub(repositoryStub);
-        let clock;
-      
-        before(function () {
-          clock = sinon.useFakeTimers();
-        });
-      
-        afterEach(function () {
-          repoSpy.search.reset();
-          clock.reset();
-        });
-      
-        after(function () {
-          clock.restore();
-        });
+    });
+  });
 
-        it('should get all instances with updates scheduled from database', function () {
-      
-          const expectedInstanceList = [];
-          expectedInstanceList.push.apply(expectedInstanceList, _.map(listOfInstances, 'data'));
-          const criteria = {
-            searchBy: {
-              type: CONST.JOB.SERVICE_INSTANCE_UPDATE
-            },
-            projection: {
-              'data.instance_id': 1
-            }
-          };
-          const paginateOpts = {
-            records: config.mongodb.record_max_fetch_count,
-            offset: config.mongodb.record_max_fetch_count
-          };
+  describe('#getInstancesWithUpdateScheduled', function () {
+    const adminController = proxyquire('../../../api-controllers/ServiceFabrikAdminController', {
+      '../common/db': {
+        Repository: Repository
+      }
+    });
+    let repoSpy = sinon.stub(repositoryStub);
+    let clock;
+
+    before(function () {
+      clock = sinon.useFakeTimers();
+    });
+
+    afterEach(function () {
+      repoSpy.search.reset();
+      clock.reset();
+    });
+
+    after(function () {
+      clock.restore();
+    });
+
+    it('should get all instances with updates scheduled from database', function () {
+
+      const expectedInstanceList = [];
+      expectedInstanceList.push.apply(expectedInstanceList, _.map(listOfInstances, 'data'));
+      const criteria = {
+        searchBy: {
+          type: CONST.JOB.SERVICE_INSTANCE_UPDATE
+        },
+        projection: {
+          'data.instance_id': 1
+        }
+      };
+      const paginateOpts = {
+        records: config.mongodb.record_max_fetch_count,
+        offset: config.mongodb.record_max_fetch_count
+      };
 
       return adminController
         .getInstancesWithUpdateScheduled()
@@ -117,6 +120,6 @@ describe('service-fabrik-admin', function () {
           expect(repoSpy.search.firstCall.args[0][1]).to.deep.equal(criteria);
           expect(repoSpy.search.firstCall.args[0][2]).to.deep.equal(paginateOpts);
         });
-        });
-        });
+    });
+  });
 });
