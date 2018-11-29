b431b73606694566e16dd3e9437597c06a8e2941
amitmalav <amitmalav96@gmail.com>
2018-11-29 15:14:33 +0530

    Fix imports

--- api-controllers/routes/index.js
+++ api-controllers/routes/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-const config = require('../common/config');
+const config = require('../../common/config');
 const _ = require('lodash');
 
 if (!_.includes(config.disabled_apis, 'broker')) {
-    exports.broker = require('./broker');
+  exports.broker = require('./broker');
 }
 if (!_.includes(config.disabled_apis, 'api')) {
-    exports.api = require('./api');
+  exports.api = require('./api');
 }
 if (!_.includes(config.disabled_apis, 'admin')) {
-    exports.admin = require('./admin');
+  exports.admin = require('./admin');
 }
 if (!_.includes(config.disabled_apis, 'manage')) {
-    exports.manage = require('./manage');
+  exports.manage = require('./manage');
 }
--- broker/server.js
+++ broker/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 console.log('Starting Service Fabrik...');
+const _ = require('lodash');
 const lib = require('./lib');
 const routes = require('../api-controllers/routes');
 const HttpServer = require('../common/HttpServer');
