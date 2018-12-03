65c8757681616dbaa6ab943de4a97f0cf6c68ecd
i350504 <jayeshk1992@gmail.com>
2018-12-03 11:01:44 +0530

    fix jshint and jsformat issue

--- api-controllers/ServiceFabrikApiController.js
+++ api-controllers/ServiceFabrikApiController.js
@@ -139,18 +139,19 @@ class ServiceFabrikApiController extends FabrikBaseController {
   }
 
   addResourceDetailsInRequest(req, res) {
+    /* jshint unused:false */
     //TODO: revisit this if default resource type changes for extension APIs
     return eventmesh.apiServerClient.getResource({
-      resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
-      resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
-      resourceId: req.params.instance_id
-    })
-    .then(resource => _.set(req, 'resourceDetails', _.get(resource, 'spec.options')))
-    .throw(new ContinueWithNext())
-    .catch(err => {
-      logger.warn(`resource could not be fetched for instance id ${req.params.instance_id}. Error: ${err}`);
-      throw(new ContinueWithNext());
-    });
+        resourceGroup: CONST.APISERVER.RESOURCE_GROUPS.DEPLOYMENT,
+        resourceType: CONST.APISERVER.RESOURCE_TYPES.DIRECTOR,
+        resourceId: req.params.instance_id
+      })
+      .then(resource => _.set(req, 'resourceDetails', _.get(resource, 'spec.options')))
+      .throw(new ContinueWithNext())
+      .catch(err => {
+        logger.warn(`resource could not be fetched for instance id ${req.params.instance_id}. Error: ${err}`);
+        throw (new ContinueWithNext());
+      });
   }
 
   verifyTenantPermission(req, res) {
@@ -166,8 +167,8 @@ class ServiceFabrikApiController extends FabrikBaseController {
     return Promise
       .try(() => {
         /* Following statement to address cross consumption scenario*/
-        let platform = _.get(req, 'body.context.platform') || _.get(req, 'query.platform') 
-        _.get(req, 'resourceDetails.context.platform') || CONST.PLATFORM.CF;
+        let platform = _.get(req, 'body.context.platform') || _.get(req, 'query.platform') ||
+          _.get(req, 'resourceDetails.context.platform') || CONST.PLATFORM.CF;
 
         if (platform === CONST.PLATFORM.SM) {
           platform = _.get(req, 'resourceDetails.context.origin') || _.get(req, 'body.context.origin') || CONST.PLATFORM.CF;
@@ -175,9 +176,9 @@ class ServiceFabrikApiController extends FabrikBaseController {
         _.set(req, 'entity.platform', platform);
 
         /*Following statement for backward compatibility*/
-        const tenantId = _.get(req, 'resourceDetails.context.tenant_id') || _.get(req, 'resourceDetails.space_guid') 
-        || _.get(req, 'body.space_guid') || _.get(req, 'query.space_guid') || _.get(req, 'query.tenant_id') 
-        || _.get(req, 'body.context.space_guid') || _.get(req, 'body.context.namespace');
+        const tenantId = _.get(req, 'resourceDetails.context.tenant_id') || _.get(req, 'resourceDetails.space_guid') ||
+          _.get(req, 'body.space_guid') || _.get(req, 'query.space_guid') || _.get(req, 'query.tenant_id') ||
+          _.get(req, 'body.context.space_guid') || _.get(req, 'body.context.namespace');
 
         if (tenantId) {
           if ((platform === CONST.PLATFORM.CF && !FabrikBaseController.uuidPattern.test(tenantId)) ||
