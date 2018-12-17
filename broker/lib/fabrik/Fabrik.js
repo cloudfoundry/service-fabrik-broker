20e8fc714f35525681b57d6935c2099a09094c7e
i350504 <jayeshk1992@gmail.com>
2018-12-17 14:39:01 +0530

    passing context while creating services

--- broker/lib/fabrik/Fabrik.js
+++ broker/lib/fabrik/Fabrik.js
@@ -7,28 +7,32 @@ const catalog = require('../../../common/models/catalog');
 const DirectorService = require('../../../operators/bosh-operator/DirectorService');
 const DockerService = require('../../../operators/docker-operator/DockerService');
 const VirtualHostService = require('../../../operators/virtualhost-operator/VirtualHostService');
+const CONST = require('../../../common/constants');
 
 class Fabrik {
   //Kept this function here as of now, we can displace it based on what we decide to do about lib/fabrik
   //review comments are requested
   static createService(plan_id, instance_id, context) {
     const plan = catalog.getPlan(plan_id);
+    const options = {
+      plan_id: plan_id,
+      context: context
+    };
     switch (plan.manager.name) {
   
       case CONST.INSTANCE_TYPE.DIRECTOR:
-        return new DirectorService(plan, instance_id);
+        return DirectorService.createInstance(instance_id, options);
   
       case CONST.INSTANCE_TYPE.DOCKER:
         if (config.enable_swarm_manager) {
-          return new DockerService(instance_id, plan);
+          return DockerService.createInstance(instance_id, options);
         } else {
           assert.fail(plan.manager.name, [CONST.INSTANCE_TYPE.DIRECTOR, CONST.INSTANCE_TYPE.VIRTUAL_HOST], undefined, 'in');
         }
         break;
   
       case CONST.INSTANCE_TYPE.VIRTUAL_HOST:
-        //space_guid is not really necessary here for dashboard rendering
-        return new VirtualHostService(instance_id, _.get(context, "space_guid"), plan);
+        return VirtualHostService.createVirtualHostService(instance_id, options);
   
       default:
         assert.fail(plan.manager.name, [CONST.INSTANCE_TYPE.DIRECTOR, CONST.INSTANCE_TYPE.DOCKER, CONST.INSTANCE_TYPE.VIRTUAL_HOST], undefined, 'in');
