309f34501e5f57a515d857165f463887415a4ea8
i068838 <s.chattopadhyay@sap.com>
2019-01-14 10:46:56 +0530

    Adding tests for renderers
    Removing unneeded code

--- interoperator/pkg/internal/renderer/gotemplate/fuctions.go
+++ interoperator/pkg/internal/renderer/gotemplate/fuctions.go
@@ -58,12 +58,6 @@ func strval(v interface{}) string {
 	switch v := v.(type) {
 	case string:
 		return v
-	case []byte:
-		return string(v)
-	case error:
-		return v.Error()
-	case fmt.Stringer:
-		return v.String()
 	default:
 		return fmt.Sprintf("%v", v)
 	}
--- interoperator/pkg/internal/renderer/gotemplate/output.go
+++ interoperator/pkg/internal/renderer/gotemplate/output.go
@@ -9,19 +9,6 @@ type gotemplateOutput struct {
 	content bytes.Buffer
 }
 
-// Manifest returns the manifest of the rendered chart as byte array.
-func (c *gotemplateOutput) Manifest() ([]byte, error) {
-	// Aggregate all valid manifests into one big doc.
-	b := bytes.NewBuffer(nil)
-	return b.Bytes(), nil
-}
-
-// ManifestAsString returns the manifest of the rendered chart as string.
-func (c *gotemplateOutput) ManifestAsString() (string, error) {
-	manifest, err := c.Manifest()
-	return string(manifest), err
-}
-
 // FileContent returns explicitly the content of the provided <filename>.
 func (c *gotemplateOutput) FileContent(filename string) (string, error) {
 	if filename == "main" {
