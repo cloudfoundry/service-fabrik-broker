e81d8e9ff6660fe5de69a302eed948136a5ff390
Murali S <muralisc@gmail.com>
2019-03-15 11:19:26 +0530

    Enrich document only if necessary

--- test/test_broker/jobs.MeteringInstanceJobs.spec.js
+++ test/test_broker/jobs.MeteringInstanceJobs.spec.js
@@ -163,9 +163,18 @@ describe('Jobs', () => {
       it('should get the instance type as director when SKU does not contain dev', () => {
         options_json.service.service_guid = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
         options_json.service.plan_guid = 'bc158c9a-7934-401e-94ab-057082a5073f';
+        options_json.service.plan = undefined;
         const dummy_event = getDummyEvent(options_json);
         const val = MeterInstanceJob.getInstanceType(dummy_event)
         expect(val).to.eql(CONST.INSTANCE_TYPE.DIRECTOR);
+      });
+      it('should get the instance type as docker when document is already enriched', () => {
+        options_json.service.service_guid = '24731fb8-7b84-4f57-914f-c3d55d793dd4';
+        options_json.service.plan_guid = 'bc158c9a-7934-401e-94ab-057082a5073f';
+        options_json.service.plan = 'dev';
+        const dummy_event = getDummyEvent(options_json);
+        const val = MeterInstanceJob.getInstanceType(dummy_event)
+        expect(val).to.eql(CONST.INSTANCE_TYPE.DOCKER);
       });
     });
 
