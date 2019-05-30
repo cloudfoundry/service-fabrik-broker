adc7ba0d5d7f2924569ddab24a09a93c6f278ec0
i068838 <s.chattopadhyay@sap.com>
2019-05-30 10:04:39 +0530

    [WIP] Interoperator Scheduler Implementation
    Introducing clusterip in sfserviceinstance
    
    Introducing sfdefautlscheduler_controller

---
+++ interoperator/pkg/controller/sfdefaultscheduler/sfdefaultscheduler_controller.go
@@ -0,0 +1,115 @@
+/*
+Copyright 2018 The Service Fabrik Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sfdefaultscheduler
+
+import (
+	"context"
+
+	osbv1alpha1 "github.com/cloudfoundry-incubator/service-fabrik-broker/interoperator/pkg/apis/osb/v1alpha1"
+	"github.com/prometheus/common/log"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new SFDefaultScheduler Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+// USER ACTION REQUIRED: update cmd/manager/main.go to call this osb.Add(mgr) to install this Controller
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileSFDefaultScheduler{Client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("sfdefaultscheduler-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to SFDefaultScheduler
+	err = c.Watch(&source.Kind{Type: &osbv1alpha1.SFServiceInstance{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create
+	// Uncomment watch a Deployment created by SFDefaultScheduler - change this for objects you create
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &osbv1alpha1.SFServiceInstance{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+var _ reconcile.Reconciler = &ReconcileSFDefaultScheduler{}
+
+// ReconcileSFDefaultScheduler reconciles a SFDefaultScheduler object
+type ReconcileSFDefaultScheduler struct {
+	client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a SFDefaultScheduler object and makes changes based on the state read
+// and what is in the SFDefaultScheduler.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
+// a Deployment as an example
+// +kubebuilder:rbac:groups=osb.servicefabrik.io,resources=sfserviceinstances,verbs=get;list;watch;create;update;patch;delete
+func (r *ReconcileSFDefaultScheduler) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	// Fetch the SFDefaultScheduler instance
+	instance := &osbv1alpha1.SFServiceInstance{}
+	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Object not found, return.  Created objects are automatically garbage collected.
+			// For additional cleanup logic use finalizers.
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+	log.Info("Watch is triggered", instance.Spec.ClusterID)
+	if instance.Spec.ClusterID == "" {
+		log.Info("Watch is triggered", instance.GetName())
+		instance.Spec.ClusterID = "1"
+		if err := r.Update(context.Background(), instance); err != nil {
+			log.Error(err, "failed to update deprovision status", "instance", instance.GetName())
+			return reconcile.Result{}, err
+		}
+	}
+	return reconcile.Result{}, nil
+}
