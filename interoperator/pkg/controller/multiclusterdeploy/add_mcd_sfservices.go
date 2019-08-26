ebf35d118e8b306398cb1e6ac5dd0e1efa135d48
i350504 <jayeshk1992@gmail.com>
2019-08-26 17:49:05 +0530

    fixing directory structure

--- interoperator/pkg/controller/multiclusterdeploy/add_mcd_sfservices.go
+++ interoperator/pkg/controller/multiclusterdeploy/add_mcd_sfservices.go
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package controller
+package multiclusterdeploy
 
 import (
 	sfservices "github.com/cloudfoundry-incubator/service-fabrik-broker/interoperator/pkg/controller/multiclusterdeploy/sfservicesreplicator"
