89fbbd4d453195a5f8b29820bbf5c985160cd54f
Vivek Anand T Kallampally <vivek.anand.t.kallampally@sap.com>
2019-09-13 13:59:21 +0530

    [interoperator] GetKubeconfig Tests

--- interoperator/pkg/apis/resource/v1alpha1/sfcluster_types_test.go
+++ interoperator/pkg/apis/resource/v1alpha1/sfcluster_types_test.go
@@ -17,12 +17,16 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"context"
+	"reflect"
 	"testing"
 
 	"github.com/onsi/gomega"
-	"golang.org/x/net/context"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/rest"
+	kubernetes "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func TestStorageSFCluster(t *testing.T) {
@@ -55,4 +59,98 @@ func TestStorageSFCluster(t *testing.T) {
 	// Test Delete
 	g.Expect(c.Delete(context.TODO(), fetched)).NotTo(gomega.HaveOccurred())
 	g.Expect(c.Get(context.TODO(), key, fetched)).To(gomega.HaveOccurred())
+}
+
+func TestSFCluster_GetKubeConfig(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+	secret := &corev1.Secret{}
+
+	type args struct {
+		c kubernetes.Client
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *rest.Config
+		wantErr bool
+		setup   func(*SFCluster)
+		cleanup func(*SFCluster)
+	}{
+		{
+			name: "should fail if secret is not found",
+			args: args{
+				c: c,
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "should fail if secret does not have kubeconfig field",
+			args: args{
+				c: c,
+			},
+			want:    nil,
+			wantErr: true,
+			setup: func(cluster *SFCluster) {
+				secret.SetResourceVersion("")
+				secret.SetName(cluster.Spec.SecretRef)
+				secret.SetNamespace(cluster.GetNamespace())
+				g.Expect(c.Create(context.TODO(), secret)).NotTo(gomega.HaveOccurred())
+			},
+			cleanup: func(cluster *SFCluster) {
+				g.Expect(c.Delete(context.TODO(), secret)).NotTo(gomega.HaveOccurred())
+			},
+		},
+		{
+			name: "should fail if kubeconfig is not valid",
+			args: args{
+				c: c,
+			},
+			want:    nil,
+			wantErr: true,
+			setup: func(cluster *SFCluster) {
+				data := make(map[string][]byte)
+				data["kubeconfig"] = []byte("foo")
+				secret.SetResourceVersion("")
+				secret.SetName(cluster.Spec.SecretRef)
+				secret.SetNamespace(cluster.GetNamespace())
+				secret.Data = data
+				g.Expect(c.Create(context.TODO(), secret)).NotTo(gomega.HaveOccurred())
+			},
+			cleanup: func(cluster *SFCluster) {
+				g.Expect(c.Delete(context.TODO(), secret)).NotTo(gomega.HaveOccurred())
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cluster := _getDummyCluster()
+			if tt.setup != nil {
+				tt.setup(cluster)
+			}
+			if tt.cleanup != nil {
+				defer tt.cleanup(cluster)
+			}
+			got, err := cluster.GetKubeConfig(tt.args.c)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SFCluster.GetKubeConfig() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SFCluster.GetKubeConfig() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func _getDummyCluster() *SFCluster {
+	return &SFCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "cluster-id",
+			Namespace: "default",
+		},
+		Spec: SFClusterSpec{
+			SecretRef: "cluster-id-secret",
+		},
+	}
 }
