1e7d68be3a6037059c5cd7e93235e94e7828856f
amitmalav <amitmalav96@gmail.com>
2019-11-13 14:49:18 +0530

    Add error desc in timeout error

--- common/errors.js
+++ common/errors.js
@@ -62,11 +62,11 @@ class Timeout extends BaseError {
     }
   }
   static timedOut(time, err, operation) {
-    const msg = `Operation ${operation ? operation + ' ' : ''}timed out after ${time} ms`;
+    const msg = `Operation ${operation ? operation + ' ' : ''}timed out after ${time} ms${_.get(err, 'description') ? ' -' + err.description : ''}`;
     return new Timeout(msg, err);
   }
   static toManyAttempts(attempts, err, operation) {
-    const msg = `Operation ${operation ? operation + ' ' : ''}failed after ${attempts} attempts`;
+    const msg = `Operation ${operation ? operation + ' ' : ''}failed after ${attempts} attempts${_.get(err, 'description') ? ' -' +  err.description : ''}`;
     return new Timeout(msg, err);
   }
 }
