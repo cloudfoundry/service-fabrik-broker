81b62ca69858a452f2f5a16dbaca697a150f07f2
Shivansh Rai <shivansh.rai@sap.com>
2019-11-27 13:14:38 +0530

    Update binding finalizers inside RetryOnConflict()

--- interoperator/controllers/provisioners/sfservicebindingcleaner/sfservicebindingcleaner_controller.go
+++ interoperator/controllers/provisioners/sfservicebindingcleaner/sfservicebindingcleaner_controller.go
@@ -59,8 +59,8 @@ func (r *ReconcileSFServiceBindingCleaner) Reconcile(req ctrl.Request) (ctrl.Res
 		}
 		err := r.Get(ctx, namespacedName, instance)
 		if err != nil && apiErrors.IsNotFound(err) {
-			binding.SetFinalizers([]string{})
 			_ = retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				binding.SetFinalizers([]string{})
 				err := c.Update(context.TODO(), binding)
 				if err != nil {
 					// The binding is possibly outdated, fetch it again and
