2ec565bf36d1793ceccd6c7370b0d53f2c38ca95
Shivansh Rai <shivansh.rai@sap.com>
2019-12-06 16:31:13 +0530

    Ensure the waitgroup gets updated when mgr.Start() fails

--- interoperator/controllers/provisioners/sfservice/sfservice_controller_suite_test.go
+++ interoperator/controllers/provisioners/sfservice/sfservice_controller_suite_test.go
@@ -80,10 +80,10 @@ func TestMain(m *testing.M) {
 func StartTestManager(mgr manager.Manager, g *gomega.GomegaWithT) (chan struct{}, *sync.WaitGroup) {
 	stop := make(chan struct{})
 	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
-		wg.Add(1)
+		defer wg.Done()
 		g.Expect(mgr.Start(stop)).NotTo(gomega.HaveOccurred())
-		wg.Done()
 	}()
 	return stop, wg
 }
