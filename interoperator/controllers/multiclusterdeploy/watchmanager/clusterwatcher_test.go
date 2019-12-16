ebda29745ef38b128ec6ee5b5a00330a5e7c673d
Vivek Anand T Kallampally <vivek.anand.t.kallampally@sap.com>
2019-12-16 12:01:43 +0530

    [interoperator] Fix Race condition in tests (#778)
    - Fix race conditions found by @shivansh

--- interoperator/controllers/multiclusterdeploy/watchmanager/clusterwatcher_test.go
+++ interoperator/controllers/multiclusterdeploy/watchmanager/clusterwatcher_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/onsi/gomega"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 )
 
@@ -21,38 +22,52 @@ func Test_clusterWatcher_start(t *testing.T) {
 
 	instanceEvents := make(chan event.GenericEvent, 1024)
 	bindingEvents := make(chan event.GenericEvent, 1024)
-	stopCh := make(chan struct{})
 	var host string
-	cw := &clusterWatcher{
-		clusterID:      "clusterID",
-		cfg:            cfg2,
-		instanceEvents: instanceEvents,
-		bindingEvents:  bindingEvents,
-		stop:           stopCh,
+	type fields struct {
+		clusterID      string
+		cfg            *rest.Config
+		timeoutSeconds int64
+		instanceEvents chan event.GenericEvent
+		bindingEvents  chan event.GenericEvent
+		stop           chan struct{}
 	}
+
 	tests := []struct {
 		name    string
-		cw      *clusterWatcher
-		setup   func()
-		cleanup func()
+		fields  fields
+		setup   func(*clusterWatcher)
+		cleanup func(*clusterWatcher)
 		wantErr bool
 	}{
 		{
 			name: "should fail to start if host is invalid",
-			cw:   cw,
-			setup: func() {
+			fields: fields{
+				clusterID:      "clusterID",
+				cfg:            cfg2,
+				instanceEvents: instanceEvents,
+				bindingEvents:  bindingEvents,
+				stop:           make(chan struct{}),
+			},
+			setup: func(cw *clusterWatcher) {
 				host = cw.cfg.Host
 				cw.cfg.Host = "hello"
 			},
-			cleanup: func() {
+			cleanup: func(cw *clusterWatcher) {
 				cw.cfg.Host = host
+				close(cw.stop)
 			},
 			wantErr: true,
 		},
 		{
 			name: "should start watching on cluster two for bindings and instances",
-			cw:   cw,
-			cleanup: func() {
+			fields: fields{
+				clusterID:      "clusterID",
+				cfg:            cfg2,
+				instanceEvents: instanceEvents,
+				bindingEvents:  bindingEvents,
+				stop:           make(chan struct{}),
+			},
+			cleanup: func(cw *clusterWatcher) {
 				instance.SetResourceVersion("")
 				binding.SetResourceVersion("")
 				g.Expect(c2.Create(context.TODO(), instance)).NotTo(gomega.HaveOccurred())
@@ -66,16 +81,21 @@ func Test_clusterWatcher_start(t *testing.T) {
 
 				g.Expect(c2.Delete(context.TODO(), binding)).NotTo(gomega.HaveOccurred())
 				g.Expect(drainAllEvents(bindingEvents, timeout)).To(gomega.Equal(1))
+				close(cw.stop)
 			},
 			wantErr: false,
 		},
 		{
 			name: "should refresh watches",
-			cw:   cw,
-			setup: func() {
-				cw.timeoutSeconds = 3
+			fields: fields{
+				clusterID:      "clusterID",
+				cfg:            cfg2,
+				timeoutSeconds: 3,
+				instanceEvents: instanceEvents,
+				bindingEvents:  bindingEvents,
+				stop:           make(chan struct{}),
 			},
-			cleanup: func() {
+			cleanup: func(cw *clusterWatcher) {
 				instance.SetResourceVersion("")
 				binding.SetResourceVersion("")
 				g.Expect(c2.Create(context.TODO(), instance)).NotTo(gomega.HaveOccurred())
@@ -89,14 +109,20 @@ func Test_clusterWatcher_start(t *testing.T) {
 
 				g.Expect(c2.Delete(context.TODO(), binding)).NotTo(gomega.HaveOccurred())
 				g.Expect(drainAllEvents(bindingEvents, timeout)).NotTo(gomega.BeZero())
-				cw.timeoutSeconds = 0
+				close(cw.stop)
 			},
 			wantErr: false,
 		},
 		{
 			name: "should stop watching on cluster when stop is closed",
-			cw:   cw,
-			cleanup: func() {
+			fields: fields{
+				clusterID:      "clusterID",
+				cfg:            cfg2,
+				instanceEvents: instanceEvents,
+				bindingEvents:  bindingEvents,
+				stop:           make(chan struct{}),
+			},
+			cleanup: func(cw *clusterWatcher) {
 				close(cw.stop)
 				g.Expect(drainAllEvents(instanceEvents, timeout)).To(gomega.BeZero())
 				g.Expect(drainAllEvents(bindingEvents, timeout)).To(gomega.BeZero())
@@ -106,13 +132,21 @@ func Test_clusterWatcher_start(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			cw := &clusterWatcher{
+				clusterID:      tt.fields.clusterID,
+				cfg:            tt.fields.cfg,
+				timeoutSeconds: tt.fields.timeoutSeconds,
+				instanceEvents: tt.fields.instanceEvents,
+				bindingEvents:  tt.fields.bindingEvents,
+				stop:           tt.fields.stop,
+			}
 			if tt.setup != nil {
-				tt.setup()
+				tt.setup(cw)
 			}
 			if tt.cleanup != nil {
-				defer tt.cleanup()
+				defer tt.cleanup(cw)
 			}
-			if err := tt.cw.start(); (err != nil) != tt.wantErr {
+			if err := cw.start(); (err != nil) != tt.wantErr {
 				t.Errorf("clusterWatcher.start() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
