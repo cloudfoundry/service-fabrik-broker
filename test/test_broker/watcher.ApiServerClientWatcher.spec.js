00e4f435d2599e4d74dd7e3d105d1594f766a81f
amitmalav <amitmalav96@gmail.com>
2020-01-21 12:13:25 +0530

    Fix UT with node version v12.13.1

--- test/test_broker/watcher.ApiServerClientWatcher.spec.js
+++ test/test_broker/watcher.ApiServerClientWatcher.spec.js
@@ -7,15 +7,18 @@ describe('WatcherRegistration', () => {
   describe('#regWatcher', () => {
     it('Should register watch on a resource', () => {
       let handlerCalled = false;
-
       function handler() {
         handlerCalled = true;
       }
+      mocks.apiServerEventMesh.nockRegisterWatcher(CONST.APISERVER.RESOURCE_GROUPS.LOCK,
+        CONST.APISERVER.RESOURCE_TYPES.DEPLOYMENT_LOCKS, {
+          labelSelector: '',
+          timeoutSeconds: 600
+        });
       return apiserver.registerWatcher(CONST.APISERVER.RESOURCE_GROUPS.LOCK,
           CONST.APISERVER.RESOURCE_TYPES.DEPLOYMENT_LOCKS, handler)
         .tap(stream => stream.write('{"foo":1}'))
         .then(stream => {
-          // expect(handlerCalled).to.eql(true);
           expect(stream).to.not.eql(null);
         });
     });
