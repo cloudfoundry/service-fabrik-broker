3e942649661fdd552fca6a768ec40b8e15bc61f5
Jayesh Kshirsagar <jayeshk1992@gmail.com>
2020-04-20 14:18:06 +0530

    take service broker from request query in outdated API (#814)

--- api-controllers/ServiceFabrikAdminController.js
+++ api-controllers/ServiceFabrikAdminController.js
@@ -276,9 +276,9 @@ class ServiceFabrikAdminController extends FabrikBaseController {
         )
         .value();
     }
-
+    const serviceBrokerName = _.get(req.query, 'service_broker', this.serviceBrokerName);
     return this
-      .findOutdatedDeployments()
+      .findOutdatedDeployments(serviceBrokerName)
       .then(deployments => {
         const mappedDeployments = _.map(deployments, mapDeployment);
         const locals = {
@@ -322,7 +322,7 @@ class ServiceFabrikAdminController extends FabrikBaseController {
       });
   }
 
-  findAllDeployments(fetchFromApiServer) {
+  findAllDeployments(fetchFromApiServer, serviceBrokerName = this.serviceBrokerName) {
     return Promise
       .all([
         this.getServiceFabrikDeployments(),
@@ -330,7 +330,7 @@ class ServiceFabrikAdminController extends FabrikBaseController {
           if (fetchFromApiServer) {
             return this.getServiceInstancesFromApiServer();
           } else {
-            return this.getServiceInstancesForServiceBroker(this.serviceBrokerName);
+            return this.getServiceInstancesForServiceBroker(serviceBrokerName);
           }
         })
       ])
@@ -342,10 +342,10 @@ class ServiceFabrikAdminController extends FabrikBaseController {
       );
   }
 
-  findOutdatedDeployments() {
-    logger.info('Searching for outdated deployments...');
+  findOutdatedDeployments(serviceBrokerName) {
+    logger.info(`Searching for outdated deployments using broker ${serviceBrokerName}...`);
     return this
-      .findAllDeployments()
+      .findAllDeployments(false, serviceBrokerName)
       .filter(deployment => {
         if (!deployment.name) {
           logger.warn(`Found service instance '${deployment.entity.name} [${deployment.metadata.guid}]' without deployment`);
