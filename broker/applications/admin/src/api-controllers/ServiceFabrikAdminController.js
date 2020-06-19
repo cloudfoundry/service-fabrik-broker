e792e5464b1bd1cbb7ce4a61a4426ea5ec2a5748
i350504 <jayeshk1992@gmail.com>
2020-06-19 10:06:09 +0530

    removing unused reporting methods from Admin controller

--- broker/applications/admin/src/api-controllers/ServiceFabrikAdminController.js
+++ broker/applications/admin/src/api-controllers/ServiceFabrikAdminController.js
@@ -38,8 +38,6 @@ const dbManager = require('../../../../data-access-layer/db/DBManager');
 const OobBackupManager = require('../../../../data-access-layer/oob-manager/OobBackupManager');
 const DirectorService = require('../../../operators/src/bosh-operator/DirectorService');
 
-const BackupReportManager = require('../../../reports/src/api-controllers/BackupReportManager');
-
 class ServiceFabrikAdminController extends FabrikBaseController {
   constructor() {
     super();
@@ -715,43 +713,6 @@ class ServiceFabrikAdminController extends FabrikBaseController {
       .then(() => res
         .status(200)
         .send({}));
-  }
-
-  // Method for backup reporting
-  getServiceInstanceBackupSummary(req, res) {
-    if (!req.params.instance_id || !req.query.start_time || !req.query.end_time) {
-      throw new BadRequest('instance_id | start_time | end_time required');
-    }
-    if (!moment(req.query.start_time, CONST.REPORT_BACKUP.INPUT_DATE_FORMAT, true).isValid()) {
-      throw new BadRequest(`Invalid start date, required format ${CONST.REPORT_BACKUP.INPUT_DATE_FORMAT}`);
-    }
-    if (!moment(req.query.end_time, CONST.REPORT_BACKUP.INPUT_DATE_FORMAT, true).isValid()) {
-      throw new BadRequest(`Invalid end date, required format ${CONST.REPORT_BACKUP.INPUT_DATE_FORMAT}`);
-    }
-    const start_time = moment.utc(req.query.start_time).toDate();
-    const end_time = moment.utc(req.query.end_time).endOf('day').toDate();
-    return BackupReportManager
-      .getInstanceBackupSummary(req.params.instance_id, start_time, end_time)
-      .then(body => res
-        .status(200)
-        .send(body));
-  }
-
-  // Method for getting backup instance ids
-  getScheduledBackupInstances(req, res) {
-    if (req.query.start_time && !moment(req.query.start_time, CONST.REPORT_BACKUP.INPUT_DATE_FORMAT, true).isValid()) {
-      throw new BadRequest(`Invalid start date, required format ${CONST.REPORT_BACKUP.INPUT_DATE_FORMAT}`);
-    }
-    if (req.query.end_time && !moment(req.query.end_time, CONST.REPORT_BACKUP.INPUT_DATE_FORMAT, true).isValid()) {
-      throw new BadRequest(`Invalid end date, required format ${CONST.REPORT_BACKUP.INPUT_DATE_FORMAT}`);
-    }
-    const start_time = req.query.start_time ? moment.utc(req.query.start_time).toDate() : undefined;
-    const end_time = req.query.end_time ? moment.utc(req.query.end_time).endOf('day').toDate() : undefined;
-    return BackupReportManager
-      .getInstancesWithBackupScheduled(start_time, end_time)
-      .then(body => res
-        .status(200)
-        .send(body));
   }
 
   startMaintenance(req, res) {
