c90180b1377251090ecaa63f9c9a829ca9168d4b
i350504 <jayeshk1992@gmail.com>
2020-07-06 14:19:38 +0530

    correcting BaseDirectorService and BaseService imports

--- broker/core/provisioner-services/src/index.js
+++ broker/core/provisioner-services/src/index.js
@@ -1,2 +1,4 @@
 'use strict';
 module.exports.DirectorService = require('./DirectorService');
+module.exports.BaseDirectorService = require('./BaseDirectorService');
+module.exports.BaseService = require('./BaseService');
