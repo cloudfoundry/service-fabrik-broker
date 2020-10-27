c9a569cd0509971ab99fe450e36e55a03979a288
Visarg Soneji <31091428+visargsoneji@users.noreply.github.com>
2020-10-27 12:37:02 +0530

    restructured UTs of quota-app, reports and scheduler (#1106)

--- broker/applications/scheduler/test/maintenance.MaintenanceManager.spec.js
+++ broker/applications/scheduler/test/maintenance.MaintenanceManager.spec.js
@@ -9,7 +9,7 @@ const {
   Repository
 } = require('@sf/common-utils');
 const config = require('@sf/app-config');
-const { maintenanceManager } = require('../../applications/scheduler/src/maintenance');
+const { maintenanceManager } = require('../src/maintenance');
 
 describe('maintenance', function () {
   /* jshint unused:false */
