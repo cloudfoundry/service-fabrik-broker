d438afdad77e64686d2932797293622ab38b63fd
Anuj Bansal <anuj.bansal16@gmail.com>
2021-01-15 02:32:35 -0800

    Returned the instance of BaseCloudClient

--- broker/data-access-layer/iaas/src/index.js
+++ broker/data-access-layer/iaas/src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const config = require('@sf/app-config');
-const CloudProviderClient = (process.env.NODE_ENV !== 'production') ? require('./CloudProviderClient') : undefined;
 const AwsClient = require('./AwsClient');
 const AzureClient = require('./AzureClient');
 const GcpClient = require('./GcpClient');
@@ -11,6 +10,9 @@ const BackupStoreForServiceInstance = require('./BackupStoreForServiceInstance')
 const BackupStoreForOob = require('./BackupStoreForOob');
 const BaseCloudClient = require('./BaseCloudClient');
 const MeteringArchiveStore = require('./MeteringArchiveStore');
+// Modifying CloudProviderClient variable as we are moving the pkgcloud to dev dependency and we dont use openstack in our production.
+// In production, it should be BaseCloudClient and in dev env its usual CloudProviderClient which uses pkgcloud.
+const CloudProviderClient = (process.env.NODE_ENV !== 'production') ? require('./CloudProviderClient') : BaseCloudClient;
 
 const getCloudClient = function (settings) {
   switch (settings.name) {
@@ -22,10 +24,7 @@ const getCloudClient = function (settings) {
       return new AwsClient(settings);
     case 'openstack':
     case 'os':
-      if(process.env.NODE_ENV !== 'production') {
-        return new CloudProviderClient(settings);
-      }
-      return undefined;
+      return new CloudProviderClient(settings);
     case 'ali':
       return new AliClient(settings);
     default:
@@ -34,9 +33,7 @@ const getCloudClient = function (settings) {
 };
 const cloudProvider = getCloudClient(config.backup.provider);
 
-if(process.env.NODE_ENV !== 'production') {
-  exports.CloudProviderClient = CloudProviderClient;
-}
+exports.CloudProviderClient = CloudProviderClient;
 exports.AwsClient = AwsClient;
 exports.AzureClient = AzureClient;
 exports.GcpClient = GcpClient;
