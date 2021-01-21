382390177932f573926bbbbe956ab0ed5191211a
i350504 <jayeshk1992@gmail.com>
2021-01-21 17:55:07 +0530

    using yarn.lock for collecting dependencies

--- broker/bin/collect-all-dependencies.js
+++ broker/bin/collect-all-dependencies.js
@@ -2,21 +2,21 @@
 
 const _ = require('lodash');
 const fs = require('fs');
+const parsers = require('@yarnpkg/parsers');
+const { exit } = require('process');
 
 const packagePath = process.argv[2];
-const rootPackagePath = process.argv[3];
+const yarnLockPath = process.argv[3];
 const destPackagePath = process.argv[4];
 
-if(_.isEmpty(packagePath) || _.isEmpty(rootPackagePath) || _.isEmpty(destPackagePath)) {
-  console.log('usage: ./collect-all-dependecies <path-to-mod-package-json> <path-to-root-package-json> <path-to-output-package-json>');
+if(_.isEmpty(packagePath) || _.isEmpty(yarnLockPath) || _.isEmpty(destPackagePath)) {
+  console.log('usage: ./collect-all-dependecies <package-path> <path-to-yarn-lock> <path-to-output-package-json>');
   return;
 }
 
 let packageSpec = JSON.parse(fs.readFileSync(packagePath));
-let rootPackageSpec = JSON.parse(fs.readFileSync(rootPackagePath));
-
-let packageDeps = packageSpec.dependencies;
-let rootDeps = rootPackageSpec.dependencies;
+let lockFileJson = parsers.parseSyml(fs.readFileSync(yarnLockPath, 'utf8'));
+let depNames = _.keys(lockFileJson);
 
 let visited = {};
 
@@ -27,29 +27,29 @@ function isSFMod(name) {
   return false;
 }
 
-function getDepsFromPackage(packagePath) {
-  return JSON.parse(fs.readFileSync(packagePath)).dependencies;
+function getDepsFromLockfile(packageName) {
+  let packageKeyName = _.find(depNames, (name) => _.startsWith(name, packageName));
+  if(!packageKeyName) exit(1);
+  return lockFileJson[packageKeyName].dependencies;
 }
 
-function collectAllDeps(modDeps, rootDeps, pathStr) {
+function collectAllDeps(modDeps) {
   let deps = {};
   for(let dep in modDeps) {
     if(isSFMod(dep)) {
       if(_.has(visited, dep)) {
-        console.log(`${pathStr},${dep}`);
         continue;
       } 
       visited[dep] = 'visited';
-      let packagePath = './' + _.split(rootDeps[dep], 'file:')[1] + '/package.json';
-      let currentModDeps = getDepsFromPackage(packagePath);
-      let tempDeps = collectAllDeps(currentModDeps, rootDeps, `${pathStr},${dep}`);
+      let currentModDeps = getDepsFromLockfile(dep);
+      let tempDeps = collectAllDeps(currentModDeps);
       if(!_.isEmpty(tempDeps)) {
         // remove duplicates from deps and tempDeps and merge
         for (let childModDep in tempDeps) {
           if(_.has(deps, childModDep)) {
             if (deps[childModDep] != tempDeps[childModDep]) {
               console.log(` Error: inconsistent versioning: ${childModDep} is present as ${deps[childModDep]} and ${tempDeps[childModDep]}`);
-              return {};
+              exit(1);
             }
           } else {
             deps[childModDep] = tempDeps[childModDep];
@@ -60,7 +60,7 @@ function collectAllDeps(modDeps, rootDeps, pathStr) {
       if(_.has(deps, dep)) {
         if (deps[dep] != modDeps[dep]) {
           console.log(` Error: inconsistent versioning: ${dep} is present as ${deps[dep]} and ${modDeps[dep]}`);
-          return {};
+          exit(1);
         }
       } else {
         deps[dep] = modDeps[dep];
@@ -70,7 +70,8 @@ function collectAllDeps(modDeps, rootDeps, pathStr) {
   return deps;
 }
 
-let deps = collectAllDeps(packageDeps, rootDeps);
+let deps = collectAllDeps(packageSpec.dependencies);
+
 packageSpec.dependencies = deps;
 const jsonData = JSON.stringify(packageSpec, null, 2);
 fs.writeFileSync(destPackagePath, jsonData);
