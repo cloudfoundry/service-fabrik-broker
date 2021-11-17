b21442c6f2765d8475cece5aedf9e52cdf96fc65
Visarg Soneji <visarg.soneji@sap.com>
2021-11-17 16:58:16 +0530

    Update plan_id label in CR (#1463)

--- broker/applications/osb-broker/src/api-controllers/ServiceBrokerApiController.js
+++ broker/applications/osb-broker/src/api-controllers/ServiceBrokerApiController.js
@@ -202,7 +202,10 @@ class ServiceBrokerApiController extends FabrikBaseController {
       res.status(statusCode).send(body);
     }
 
-    const labels = req.params.region ? { 'region':req.params.region } : undefined;
+    let labels = req.params.region ? { 'region':req.params.region } : undefined;
+    labels = _.merge(labels, {
+      plan_id : planId
+    });
     
     _.forIn(labels, function(value, key) {
       if (!isValidKubernetesLabelValue(value)) {
